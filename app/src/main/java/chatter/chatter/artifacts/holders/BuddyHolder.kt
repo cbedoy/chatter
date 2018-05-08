2f452c7dbf86de4af14f1f4dac726671aadea872
Bedoy <carlos.bedoy@gmail.com>
2018-05-07 22:20:56 -0500

    Added buddy entity

---
+++ app/src/main/java/chatter/chatter/artifacts/holders/BuddyHolder.kt
@@ -0,0 +1,23 @@
+package chatter.chatter.artifacts.holders
+
+import android.view.View
+import chatter.chatter.artifacts.BaseModel
+import chatter.chatter.artifacts.BaseViewHolder
+import chatter.chatter.artifacts.models.Buddy
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.buddy_holder.*
+
+/**
+ * Chatter
+ *
+ * Created by bedoy on 5/7/18.
+ */
+class BuddyHolder(override val containerView: View?) : BaseViewHolder(containerView) {
+    override fun reloadData(model: BaseModel) {
+        model as Buddy
+
+        Glide.with(itemView?.context).load(model.avatar).into(buddy_holder_avatar)
+
+        buddy_holder_title.text = model.nickname
+    }
+}
---
+++ app/src/main/java/chatter/chatter/artifacts/models/Buddy.kt
@@ -0,0 +1,22 @@
+package chatter.chatter.artifacts.models
+
+import android.view.View
+import chatter.chatter.artifacts.BaseModel
+
+/**
+ * Chatter
+ *
+ * Created by bedoy on 5/7/18.
+ */
+class Buddy : BaseModel {
+
+    var identifier: String? = null
+    var nickname: String? = null
+    var textColor: Int? = null
+    var listener: View.OnClickListener? = null
+    var avatar: String? = null
+
+    override fun getType(): Int {
+        return BaseModel.TYPE.BUDDY.value
+    }
+}
